f1030f27053cd84a08320b885b302c85cba8410d
GamerEmilianoFF <95982260+GamerEmilianoFF@users.noreply.github.com>
2024-03-25 16:39:58 -0500

    Delete theow.lua

--- theow.lua
+++
@@ -1,9 +0,0 @@
-local annoying = {"IISpace_0234II", "baneedhoodemi", "Nul404X"}
- 
-game.Players.PlayerAdded:Connect(function(plr)
-for i, v in pairs(annoying) do
-if plr.Name == v then
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GamerEmilianoFF/scripts/main/notiow.lua", true))()
-end
-end
-end)
